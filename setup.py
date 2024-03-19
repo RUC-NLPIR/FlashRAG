0f9410c0884185f29e686544a7d3541dedc17fe6
ignorejjj <1009979434@qq.com>
2024-03-19 22:45:03 +0800

    Update setup

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     description='A library for efficient Retrieval-Augmented Generation research',
     long_description=long_description,
     long_description_content_type="text/markdown",
+    include_package_data=True,
     install_requires=requirements,
     python_requires='>=3.8',
 )
