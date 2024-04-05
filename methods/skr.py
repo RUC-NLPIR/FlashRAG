3ba940c55517015e960ce37f0230895b9718cc64
ignorejjj <1009979434@qq.com>
2024-04-05 16:08:14 +0800

    Update: add training sample in skr

--- methods/skr.py
+++ methods/skr.py
@@ -3,6 +3,20 @@ Reference:
     Yile Wang et al. "Self-Knowledge Guided Retrieval Augmentation for Large Language Models"
     in EMNLP Findings 2023.
     Official repo: https://github.com/THUNLP-MT/SKR/
+
+Note:
+    `skr-knn` need training data in inference stage to determain whether to retrieve. training data should in
+    `.json` format in following format:
+    format: 
+        [
+            {
+                "question": ... ,  // question
+                "judgement": "ir_better" / "ir_worse" / "same",  // judgement result, can be obtained by comparing 
+                ...
+            },
+            ...
+        ]
+
 """
 
 from flashrag.config import Config
@@ -13,10 +27,12 @@ from flashrag.pipeline import ConditionalPipeline
 # ###### Specified parameters ######
 judger_name = 'skr'
 model_path = 'princeton-nlp/sup-simcse-bert-base-uncased'
+training_data_path = './sample_data/skr_training.json'
 
 config_dict = {
     'judger_name': judger_name,
-    'judger_model_path': model_path
+    'judger_model_path': model_path,
+    'judger_training_data_path': training_data_path
 }
 
 
