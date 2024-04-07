811c7f68b28a705fc882615c4aa3605b5af80692
ignorejjj <1009979434@qq.com>
2024-04-07 11:25:00 +0800

    fix bug

--- examples/quick_start/test_pipeline.py
+++ examples/quick_start/test_pipeline.py
@@ -1,8 +1,6 @@
 import argparse
 from flashrag.config import Config
 from flashrag.utils import get_dataset
-from flashrag.pipeline import SequentialPipeline
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_path', type=str)
@@ -17,11 +15,13 @@ config_dict = {
             }
 
 config = Config(config_dict = config_dict)
+
+from flashrag.pipeline import SequentialPipeline
 all_split = get_dataset(config)
 test_data = all_split['test']
 pipeline = SequentialPipeline(config)
 
-output_dataset, result = pipeline.run(test_data)
+output_dataset, result = pipeline.run(test_data,do_eval=True)
 print("---evaluation result---")
 print(result)
 print("---generation output---")
