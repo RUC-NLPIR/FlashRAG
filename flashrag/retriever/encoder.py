b5c50633cca5f6c33a1fb346996a4973fd492ce3
ignorejjj <1009979434@qq.com>
2024-04-26 21:40:47 +0800

    Update: modify code

---
+++ flashrag/retriever/encoder.py
@@ -0,0 +1,59 @@
+import torch
+import numpy as np
+from transformers import AutoTokenizer, AutoModel
+from flashrag.retriever.utils import load_model, pooling
+
+class Encoder:
+    def __init__(self, model_name, model_path, pooling_method, max_length, use_fp16):
+        self.model_name = model_name
+        self.model_path = model_path
+        self.pooling_method = pooling_method
+        self.max_length = max_length
+        self.use_fp16 = use_fp16
+
+        self.model, self.tokenizer = load_model(model_path = model_path, 
+                                                  use_fp16 = use_fp16)
+        
+    @torch.no_grad()
+    def encode(self, query_list, is_query=True) -> np.ndarray:
+        # processing query for different encoders
+        if isinstance(query_list, str):
+            query_list = [query_list]
+
+        if "e5" in self.model_name.lower():
+            if is_query:
+                query_list = [f"query: {query}" for query in query_list]
+            else:
+                query_list = [f"passage: {query}" for query in query_list]
+
+        inputs = self.tokenizer(query_list, 
+                                max_length = self.max_length, 
+                                padding = True, 
+                                truncation = True, 
+                                return_tensors = "pt"
+                            )
+        inputs = {k: v.cuda() for k, v in inputs.items()}
+
+        #TODO: support encoder-only T5 model
+        if "T5" in type(self.model).__name__:
+            # T5-based retrieval model
+            decoder_input_ids = torch.zeros(
+                (inputs['input_ids'].shape[0], 1), dtype=torch.long
+            ).to(inputs['input_ids'].device)
+            output = self.model(
+                **inputs, decoder_input_ids=decoder_input_ids, return_dict=True
+            )
+            query_emb = output.last_hidden_state[:, 0, :]
+
+        else:
+            output = self.model(**inputs, return_dict=True)
+            query_emb = pooling(output.pooler_output, 
+                                output.last_hidden_state, 
+                                inputs['attention_mask'],
+                                self.pooling_method)
+            if  "dpr" not in self.model_name.lower():
+                query_emb = torch.nn.functional.normalize(query_emb, dim=-1)
+
+        query_emb = query_emb.detach().cpu().numpy()
+        query_emb = query_emb.astype(np.float32, order="C")
+        return query_emb
--- flashrag/retriever/retriever.py
+++ flashrag/retriever/retriever.py
@@ -17,7 +17,7 @@ import torch.nn.functional as F
 
 from transformers import AutoTokenizer, AutoModel
 from flashrag.retriever.utils import load_model, pooling, base_content_function, load_corpus, load_docs
-
+from flashrag.retriever.encoder import Encoder
 
 
 def cache_manager(func):
@@ -190,63 +190,22 @@ class DenseRetriever(BaseRetriever):
         self.index = faiss.read_index(self.index_path)
         #self.index = faiss.index_cpu_to_all_gpus(self.index)
         self.corpus = load_corpus(self.corpus_path)
-        self.encoder, self.tokenizer = load_model(model_path = config['retrieval_model_path'], 
-                                                  use_fp16 = config['retrieval_use_fp16'])
+        self.encoder = Encoder(
+             model_name = self.retrieval_method, 
+             model_path = config['retrieval_model_path'],
+             pooling_method = config['retrieval_pooling_method'],
+             max_length = config['retrieval_query_max_length'],
+             use_fp16 = config['retrieval_use_fp16']
+            )
         self.topk = config['retrieval_topk']
-        self.pooling_method = self.config['retrieval_pooling_method'] 
-        self.query_max_length = self.config['retrieval_query_max_length']
         self.batch_size = self.config['retrieval_batch_size']
 
-    
-    @torch.no_grad()
-    def _encode(self, query_list, is_query=True) -> np.ndarray:
-        # processing query for different encoders
-        if isinstance(query_list, str):
-            query_list = [query_list]
-
-        if "e5" in self.retrieval_method.lower():
-            if is_query:
-                query_list = [f"query: {query}" for query in query_list]
-            else:
-                query_list = [f"passage: {query}" for query in query_list]
-
-        inputs = self.tokenizer(query_list, 
-                                max_length = self.query_max_length, 
-                                padding = True, 
-                                truncation = True, 
-                                return_tensors = "pt"
-                            )
-        inputs = {k: v.cuda() for k, v in inputs.items()}
-
-        #TODO: support encoder-only T5 model
-        if "T5" in type(self.encoder).__name__:
-            # T5-based retrieval model
-            decoder_input_ids = torch.zeros(
-                (inputs['input_ids'].shape[0], 1), dtype=torch.long
-            ).to(inputs['input_ids'].device)
-            output = self.encoder(
-                **inputs, decoder_input_ids=decoder_input_ids, return_dict=True
-            )
-            query_emb = output.last_hidden_state[:, 0, :]
-
-        else:
-            output = self.encoder(**inputs, return_dict=True)
-            query_emb = pooling(output.pooler_output, 
-                                output.last_hidden_state, 
-                                inputs['attention_mask'],
-                                self.pooling_method)
-            if  "dpr" not in self.retrieval_method:
-                query_emb = torch.nn.functional.normalize(query_emb, dim=-1)
-
-        query_emb = query_emb.detach().cpu().numpy()
-        query_emb = query_emb.astype(np.float32, order="C")
-        return query_emb
 
     @cache_manager
     def search(self, query, num: int = None, return_score = False):
         if num is None:
             num = self.topk
-        query_emb = self._encode(query)
+        query_emb = self.encoder.encode(query)
         scores, idxs = self.index.search(query_emb, k=num)
         idxs = idxs[0]
         scores = scores[0]
@@ -271,7 +230,7 @@ class DenseRetriever(BaseRetriever):
 
         for start_idx in tqdm(range(0, len(query_list), batch_size), desc='Retrieval process: '):
             query_batch = query_list[start_idx:start_idx + batch_size]
-            batch_emb = self._encode(query_batch)
+            batch_emb = self.encoder.encode(query_batch)
             batch_scores, batch_idxs = self.index.search(batch_emb, k=num)
             batch_scores = batch_scores.tolist()
             batch_idxs = batch_idxs.tolist()
