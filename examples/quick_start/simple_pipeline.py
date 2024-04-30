98226f046e9bfe8f1047e4d94887243535e221e6
ignorejjj <1009979434@qq.com>
2024-04-30 16:59:18 +0800

    Fix bug

--- examples/quick_start/simple_pipeline.py
+++ examples/quick_start/simple_pipeline.py
@@ -2,6 +2,7 @@ import argparse
 from flashrag.config import Config
 from flashrag.utils import get_dataset
 from flashrag.pipeline import SequentialPipeline
+from flashrag.prompt import PromptTemplate
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_path', type=str)
@@ -14,14 +15,21 @@ config_dict = {
                 'corpus_path': 'indexes/sample_data.jsonl',
                 'retriever_model2path': {'e5': args.retriever_path},
                 'generator_model2path': {'llama2-7B-chat': args.model_path},
-                'metrics': ['em','f1','sub_em']
+                'metrics': ['em','f1','sub_em'],
+                'retrieval_topk': 1,
+                'save_intermediate_data': True
             }
 
 config = Config(config_dict = config_dict)
 
 all_split = get_dataset(config)
 test_data = all_split['test']
-pipeline = SequentialPipeline(config)
+prompt_templete = PromptTemplate(
+    config, 
+    system_prompt = "Answer the question based on the given document. Only give me the answer and do not output any other words.\nThe following are given documents.\n\n{reference}",
+    user_prompt = "Question: {question}\nAnswer:"
+)
+pipeline = SequentialPipeline(config, prompt_template=prompt_templete)
 
 output_dataset = pipeline.run(test_data,do_eval=True)
 print("---generation output---")
