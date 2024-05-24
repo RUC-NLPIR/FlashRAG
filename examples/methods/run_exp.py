e4ad7e2394f38bb48d559f54699060f7933a862e
ignorejjj <1009979434@qq.com>
2024-05-24 10:13:38 +0800

    Modify supporting works

--- examples/methods/run_exp.py
+++ examples/methods/run_exp.py
@@ -42,6 +42,12 @@ def zero_shot(args):
     result = pipeline.naive_run(test_data)
 
 def aar(args):
+    """
+    Reference:
+        Zichun Yu et al. "Augmentation-Adapted Retriever Improves Generalization of Language Models as Generic Plug-In"
+        in ACL 2023.
+        Official repo: https://github.com/OpenMatch/Augmentation-Adapted-Retriever
+    """
     # two types of checkpoint: ance / contriever
     #retrieval_method = "AAR-contriever"  # AAR-ANCE
     # index path of this retriever
@@ -78,6 +84,14 @@ def aar(args):
     result = pipeline.run(test_data)
 
 def llmlingua(args):
+    """
+    Reference:
+        Huiqiang Jiang et al. "LLMLingua: Compressing Prompts for Accelerated Inference of Large Language Models"
+        in EMNLP 2023
+        Huiqiang Jiang et al. "LongLLMLingua: Accelerating and Enhancing LLMs in Long Context Scenarios via Prompt Compression"
+        in ICLR MEFoMo 2024.
+        Official repo: https://github.com/microsoft/LLMLingua
+    """
     refiner_name = "longllmlingua" # 
     refiner_model_path = "model/llama-2-7b-hf"
 
@@ -109,6 +123,12 @@ def llmlingua(args):
     result = pipeline.run(test_data)
 
 def recomp(args):
+    """
+    Reference:
+        Fangyuan Xu et al. "RECOMP: Improving Retrieval-Augmented LMs with Compression and Selective Augmentation"
+        in ICLR 2024.
+        Official repo: https://github.com/carriex/recomp
+    """
     # ###### Specified parameters ######
     refiner_name = "recomp-abstractive" # recomp-extractive
     model_dict = {'nq':"model/recomp_nq_abs",
@@ -148,6 +168,21 @@ def recomp(args):
     result = pipeline.run(test_data)
 
 def sc(args):
+    """
+    Reference:
+        Yucheng Li et al. "Compressing Context to Enhance Inference Efficiency of Large Language Models"
+        in EMNLP 2023.
+        Official repo: https://github.com/liyucheng09/Selective_Context
+
+    Note: 
+        Need to install spacy:
+            ```python -m spacy download en_core_web_sm```
+        or 
+            ```
+            wget https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.6.0/en_core_web_sm-3.6.0.tar.gz  
+            pip install en_core_web_sm-3.6.0.tar.gz
+            ```
+    """
     refiner_name = "selective-context"  
     refiner_model_path = "model/gpt2"
 
@@ -173,6 +208,12 @@ def sc(args):
     result = pipeline.run(test_data)
 
 def retrobust(args):
+    """
+    Reference:
+        Ori Yoran et al. "Making Retrieval-Augmented Language Models Robust to Irrelevant Context"
+        in ICLR 2024.
+        Official repo: https://github.com/oriyor/ret-robust
+    """
     model_dict = {'nq':'model/llama-2-13b-peft-nq-retrobust',
                   '2wiki':'model/llama-2-13b-peft-2wikihop-retrobust'}
     if args.dataset_name == 'triviaqa':
@@ -199,6 +240,12 @@ def retrobust(args):
     result = pipeline.run(test_data, pred_process_fun=selfask_pred_parse)
 
 def sure(args):
+    """
+    Reference:
+        Jaehyung Kim et al. "SuRe: Summarizing Retrievals using Answer Candidates for Open-domain QA of LLMs"
+        in ICLR 2024
+        Official repo: https://github.com/bbuing9/ICLR24_SuRe
+    """
     config_dict = {
             'save_note': 'SuRe',
             'gpu_id':args.gpu_id,
@@ -214,6 +261,10 @@ def sure(args):
     result = pipeline.run(test_data)
 
 def replug(args):
+    """
+    Reference:
+        Weijia Shi et al. "REPLUG: Retrieval-Augmented Black-Box Language Models".
+    """
     save_note = 'replug'
     config_dict = {'save_note': save_note,
                 'gpu_id':args.gpu_id,
@@ -230,6 +281,26 @@ def replug(args):
     result = pipeline.run(test_data)
 
 def skr(args):
+    """
+    Reference:
+        Yile Wang et al. "Self-Knowledge Guided Retrieval Augmentation for Large Language Models"
+        in EMNLP Findings 2023.
+        Official repo: https://github.com/THUNLP-MT/SKR/
+
+    Note:
+        `skr-knn` need training data in inference stage to determain whether to retrieve. training data should in
+        `.json` format in following format:
+        format: 
+            [
+                {
+                    "question": ... ,  // question
+                    "judgement": "ir_better" / "ir_worse" / "same",  // judgement result, can be obtained by comparing 
+                    ...
+                },
+                ...
+            ]
+
+    """
     judger_name = 'skr'
     model_path = 'model/sup-simcse-bert-base-uncased'
     training_data_path = './sample_data/skr_training.json'
@@ -255,6 +326,12 @@ def skr(args):
     result = pipeline.run(test_data)
 
 def selfrag(args):
+    """
+    Reference:
+        Akari Asai et al. " SELF-RAG: Learning to Retrieve, Generate and Critique through self-reflection"
+        in ICLR 2024.
+        Official repo: https://github.com/AkariAsai/self-rag
+    """
     config_dict = {'generator_model':'selfrag-llama2-7B',
                 'generator_model_path': 'model/selfrag_llama2_7b',
                'framework': 'vllm',
@@ -276,6 +353,13 @@ def selfrag(args):
     result = pipeline.run(test_data, batch_size=256)
 
 def flare(args):
+    """
+    Reference:
+        Zhengbao Jiang et al. "Active Retrieval Augmented Generation"
+        in EMNLP 2023.
+        Official repo: https://github.com/bbuing9/ICLR24_SuRe
+
+    """
     config_dict={'save_note':'flare', 'gpu_id':args.gpu_id, 
                 'dataset_name':args.dataset_name}
     config = Config('my_config.yaml',config_dict)
@@ -288,6 +372,15 @@ def flare(args):
     result = pipeline.run(test_data)
 
 def iterretgen(args):
+    """
+    Reference:
+        Zhihong Shao et al. "Enhancing Retrieval-Augmented Large Language Models with Iterative
+                            Retrieval-Generation Synergy"
+        in EMNLP Findings 2023.
+
+        Zhangyin Feng et al. "Retrieval-Generation Synergy Augmented Large Language Models"
+        in EMNLP Findings 2023. 
+    """
     iter_num = 3
     config_dict = {'save_note': 'iter-retgen',
                 'gpu_id':args.gpu_id,
