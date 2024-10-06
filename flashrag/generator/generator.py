f4b94004d52d900741a6a0bfc3323d8baccc39a3
Yijie Xu <yxu409@connect.hkust-gz.edu.cn>
2024-10-06 16:51:37 +0000

    [Edit] add support for flash attention 2 in HFCausalLMGenerator, and add llama 3 extra support

--- flashrag/generator/generator.py
+++ flashrag/generator/generator.py
@@ -297,6 +297,7 @@ class HFCausalLMGenerator(BaseGenerator):
                 self.model_path,
                 torch_dtype="auto",
                 device_map="auto",
+                attn_implementation="flash_attention_2",
                 trust_remote_code=True,
             )
         else:
@@ -307,6 +308,8 @@ class HFCausalLMGenerator(BaseGenerator):
         )
         if "qwen" not in self.model_name:
             tokenizer.pad_token = tokenizer.eos_token
+        if "Llama-3" in self.model_name:
+            tokenizer.pad_token_id = tokenizer.eos_token_id
         tokenizer.padding_side = "left"
 
         return model, tokenizer
