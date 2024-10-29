9996dbeda6befa73d22d382016b0e2b6481b9f5b
ignorejjj <1009979434@qq.com>
2024-10-29 14:39:48 +0800

    update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extras_require['full'] = sum(extras_require.values(), [])
 
 setup(
     name="flashrag-dev",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     url="https://github.com/RUC-NLPIR/FlashRAG",
     license="MIT License",
@@ -27,5 +27,5 @@ setup(
     package_data={"flashrag-dev": ["/config/basic_config.yaml"]},
     install_requires=extras_require['core'],
     extras_require=extras_require,
-    python_requires=">=3.8",
+    python_requires=">=3.9",
 )
