54c20188a032a9f085e1fd3896a1c8cd976738e8
ignorejjj <1009979434@qq.com>
2024-11-10 16:21:32 +0800

    feat: add docstrings for Encoder and STEncoder classes and improve inference mode usage

--- flashrag/retriever/encoder.py
+++ flashrag/retriever/encoder.py
@@ -5,6 +5,21 @@ from flashrag.retriever.utils import load_model, pooling, parse_query
 
 
 class Encoder:
+    """
+    Encoder class for encoding queries using a specified model.
+
+    Attributes:
+        model_name (str): The name of the model.
+        model_path (str): The path to the model.
+        pooling_method (str): The method used for pooling.
+        max_length (int): The maximum length of the input sequences.
+        use_fp16 (bool): Whether to use FP16 precision.
+        instruction (str): Additional instructions for parsing queries.
+
+    Methods:
+        encode(query_list: List[str], is_query=True) -> np.ndarray:
+            Encodes a list of queries into embeddings.
+    """
     def __init__(self, model_name, model_path, pooling_method, max_length, use_fp16, instruction):
         self.model_name = model_name
         self.model_path = model_path
@@ -15,7 +30,7 @@ class Encoder:
 
         self.model, self.tokenizer = load_model(model_path=model_path, use_fp16=use_fp16)
 
-    @torch.inference_mode(mode=True)
+    @torch.inference_mode()
     def encode(self, query_list: List[str], is_query=True) -> np.ndarray:
         query_list = parse_query(self.model_name, query_list, self.instruction)
 
@@ -44,6 +59,22 @@ class Encoder:
 
 
 class STEncoder:
+    """
+    STEncoder class for encoding queries using SentenceTransformers.
+
+    Attributes:
+        model_name (str): The name of the model.
+        model_path (str): The path to the model.
+        max_length (int): The maximum length of the input sequences.
+        use_fp16 (bool): Whether to use FP16 precision.
+        instruction (str): Additional instructions for parsing queries.
+
+    Methods:
+        encode(query_list: List[str], batch_size=64, is_query=True) -> np.ndarray:
+            Encodes a list of queries into embeddings.
+        multi_gpu_encode(query_list: List[str], is_query=True, batch_size=None) -> np.ndarray:
+            Encodes a list of queries into embeddings using multiple GPUs.
+    """
     def __init__(self, model_name, model_path, max_length, use_fp16, instruction):
         import torch
         from sentence_transformers import SentenceTransformer
@@ -57,7 +88,7 @@ class STEncoder:
             model_path, model_kwargs={"torch_dtype": torch.float16 if use_fp16 else torch.float}
         )
 
-    @torch.inference_mode(mode=True)
+    @torch.inference_mode()
     def encode(self, query_list: List[str], batch_size=64, is_query=True) -> np.ndarray:
         query_list = parse_query(self.model_name, query_list, self.instruction)
         query_emb = self.model.encode(
@@ -67,7 +98,7 @@ class STEncoder:
 
         return query_emb
 
-    @torch.inference_mode(mode=True)
+    @torch.inference_mode()
     def multi_gpu_encode(self, query_list: List[str], is_query=True, batch_size=None) -> np.ndarray:
         query_list = parse_query(self.model_name, query_list, self.instruction)
         pool = self.model.start_multi_process_pool()
@@ -75,6 +106,6 @@ class STEncoder:
             query_list, pool, convert_to_numpy=True, normalize_embeddings=True, batch_size=batch_size,show_progress_bar=True
         )
         self.model.stop_multi_process_pool(pool)
-        query_emb.astype(np.float32, order="C")
+        query_emb = query_emb.astype(np.float32, order="C")
 
         return query_emb
