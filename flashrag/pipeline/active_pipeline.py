82dca4c491302e7bcd673bfe1dcaaa7a971da6e6
ignorejjj <1009979434@qq.com>
2025-01-07 16:26:49 +0800

    fix typo

--- flashrag/pipeline/active_pipeline.py
+++ flashrag/pipeline/active_pipeline.py
@@ -98,7 +98,7 @@ class SelfRAGPipeline(BasicPipeline):
     def __init__(
         self,
         config,
-        threhsold=0.2,
+        threshold=0.2,
         max_depth=2,
         beam_width=2,
         w_rel=1.0,
@@ -135,7 +135,7 @@ class SelfRAGPipeline(BasicPipeline):
                 config, user_prompt="### Instruction:\n" + question_inst + "\n\n### Response:\n", enable_chat=False
             )
 
-        self.threshold = threhsold
+        self.threshold = threshold
         self.max_depth = max_depth
         self.beam_width = beam_width
         self.w_rel, self.w_sup, self.w_use = w_rel, w_sup, w_use
@@ -926,7 +926,9 @@ class IRCOTPipeline(BasicPipeline):
     IRCOT_INSTRUCTION = 'You serve as an intelligent assistant, adept at facilitating users through complex, multi-hop reasoning across multiple documents. This task is illustrated through demonstrations, each consisting of a document set paired with a relevant question and its multi-hop reasoning thoughts. Your task is to generate one thought for current step, DON\'T generate the whole thoughts at once! If you reach what you believe to be the final step, start with "So the answer is:".'
     IRCOT_EXAMPLE = "Wikipedia Title: Kurram Garhi\nKurram Garhi is a small village located near the city of Bannu, which is the part of Khyber Pakhtunkhwa province of Pakistan. Its population is approximately 35000. Barren hills are near this village. This village is on the border of Kurram Agency. Other nearby villages are Peppal, Surwangi and Amandi Kala.\n\nWikipedia Title: 2001–02 UEFA Champions League second group stage\nEight winners and eight runners- up from the first group stage were drawn into four groups of four teams, each containing two group winners and two runners- up. Teams from the same country or from the same first round group could not be drawn together. The top two teams in each group advanced to the quarter- finals.\n\nWikipedia Title: Satellite tournament\nA satellite tournament is either a minor tournament or event on a competitive sporting tour or one of a group of such tournaments that form a series played in the same country or region.\n\nWikipedia Title: Trojkrsti\nTrojkrsti is a village in Municipality of Prilep, Republic of Macedonia.\n\nWikipedia Title: Telephone numbers in Ascension Island\nCountry Code:+ 247< br> International Call Prefix: 00 Ascension Island does not share the same country code( +290) with the rest of St Helena.\n\nQuestion: Are both Kurram Garhi and Trojkrsti located in the same country?\nThought: Kurram Garhi is located in the country of Pakistan. Trojkrsti is located in the country of Republic of Macedonia. Thus, they are not in the same country. So the answer is: no.\n\n"
 
-    def __init__(self, config, prompt_template=None, max_iter=2, retriever=None, generator=None):
+    def __init__(
+        self, config, prompt_template=None, max_iter=2, retriever=None, generator=None
+    ):
         # if not provide prompt template, use default template provided by IRCOT
         if prompt_template is None:
             prompt_template = PromptTemplate(
